584f1c01ef0871b802f1586652e16522f9366a57
Ilina Evgeniya <lopute44@gmail.com>
2020-07-31 08:51:11 +0700

    added my test

---
+++ test/src/test/java/MyFirstTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("https://www.google.com/");
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+
+}
